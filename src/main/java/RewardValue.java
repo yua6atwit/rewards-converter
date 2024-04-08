519535ba6fba302454f4d26fde327f5d45349e54
Annie Yu <yua6@wit.edu>
2024-04-08 16:50:46 -0400

    added RewardValue.java

---
+++ src/main/java/RewardValue.java
@@ -0,0 +1,31 @@
+    
+public class RewardValue {
+
+    private double cashValue;
+    final static double mileToCash = 0.0035;
+
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public RewardValue(int milesValue) {
+        this.cashValue = convertToCash(milesValue);
+    }
+
+    public Integer getMilesValue() {
+        return convertToMiles(cashValue);
+    }
+
+    public Double getCashValue() {
+        return cashValue;
+    }
+
+    private static int convertToMiles(double cashValue) {
+        return (int) (cashValue / mileToCash);
+    }
+
+    private static double convertToCash(int milesValue) {
+        return milesValue*mileToCash;
+    }
+
+}
